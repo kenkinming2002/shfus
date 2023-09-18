70752d1a87f09cff3d88b97942f95d1ffc94b709
Kwok Kin Ming <kenkinming2002@gmail.com>
2023-09-19 02:08:45 +0800

    Initial Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,43 @@
-#[rocket::get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket::fs::NamedFile;
+use rocket::fs::TempFile;
+use rocket::fs::relative;
+
+use rocket::http::Status;
+
+use rocket::get;
+use rocket::post;
+
+use rocket::form::FromForm;
+use rocket::form::Form;
+
+use rocket::response::Redirect;
+
+use std::path::Path;
+
+#[get("/")]
+async fn root() -> Redirect {
+    Redirect::permanent("/index.html")
+}
+
+#[get("/index.html")]
+async fn index() -> Result<NamedFile, Status> {
+    NamedFile::open(relative!("statics/index.html")).await.ok().ok_or(Status::InternalServerError)
+}
+
+#[derive(FromForm)]
+struct Upload<'r> {
+    file: TempFile<'r>,
+}
+
+#[post("/upload", data = "<upload>")]
+async fn upload(mut upload: Form<Upload<'_>>) -> Result<NamedFile, Status> {
+    let name = upload.file.name().ok_or(Status::BadRequest)?.to_owned();
+    let path = Path::new(relative!("uploads")).join(&name);
+    upload.file.move_copy_to(&path).await.ok().ok_or(Status::BadRequest)?;
+    NamedFile::open(relative!("statics/upload.html")).await.ok().ok_or(Status::InternalServerError)
 }
 
 #[rocket::launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", rocket::routes![index])
+    rocket::build().mount("/", rocket::routes![root, index, upload])
 }
