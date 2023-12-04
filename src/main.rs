460354377bccfd2a9a32d078417216dbd2a3e92b
Kwok Kin Ming <kenkinming2002@gmail.com>
2023-12-05 00:34:31 +0800

    Better HTTP Respond

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use rocket::fs::FileServer;
-use rocket::fs::NamedFile;
 use rocket::fs::TempFile;
 use rocket::fs::relative;
 use rocket::fs::Options;
 
+use rocket::response;
+use rocket::response::Response;
+use rocket::response::Responder;
+
 use rocket::http::Status;
+use rocket::http::Header;
+use rocket::http::ContentType;
 
 use rocket::post;
 
 use rocket::form::FromForm;
 use rocket::form::Form;
 
+use std::io::Cursor;
 use std::path::Path;
 
 #[derive(FromForm)]
@@ -18,16 +24,41 @@ struct Upload<'r> {
     files: Vec<TempFile<'r>>,
 }
 
+enum UploadResponder {
+    BadUpload(&'static str),
+    Success,
+}
+
+impl<'r> Responder<'r, 'static> for UploadResponder {
+    fn respond_to(self, _: &'r rocket::Request<'_>) -> response::Result<'static> {
+        match self {
+            Self::BadUpload(message) => Response::build()
+                .status(Status::BadRequest)
+                .header(ContentType::Plain)
+                .sized_body(message.len(), Cursor::new(message))
+                .ok(),
+            Self::Success => Response::build()
+                .status(Status::SeeOther)
+                .header(Header::new("Location", "index.html"))
+                .ok(),
+        }
+    }
+}
+
 #[post("/upload", data = "<upload>")]
-async fn upload(mut upload: Form<Upload<'_>>) -> Result<NamedFile, Status> {
+async fn upload(mut upload: Form<Upload<'_>>) -> UploadResponder {
     for file in &mut upload.files {
         // 1: Sanitize the filename and preserve the file extension
-        let name = file.raw_name().ok_or(Status::BadRequest)?;
+        let name = match file.raw_name().ok_or(Status::BadRequest) {
+            Ok(name) => name,
+            Err(_) => return UploadResponder::BadUpload("Missing filename"),
+        };
+
         let name = name.dangerous_unsafe_unsanitized_raw();
         let name = name.as_str();
         let name = name.chars().filter(|c| !std::path::is_separator(*c)).collect::<String>();
         if name.is_empty() {
-            return Err(Status::BadRequest);
+            return UploadResponder::BadUpload("Empty filename");
         }
 
         // 2: Compute the upload path
@@ -35,12 +66,14 @@ async fn upload(mut upload: Form<Upload<'_>>) -> Result<NamedFile, Status> {
         let path = path.join(name);
 
         // 3: Persist the file
-        file.move_copy_to(&path).await.ok().ok_or(Status::BadRequest)?;
+        if let Err(_) = file.move_copy_to(&path).await {
+            return UploadResponder::BadUpload("Failed to persist some file");
+        }
 
         // 4: Done
         eprintln!("File uploaded to {path}", path = path.display());
     }
-    NamedFile::open(relative!("statics/upload.html")).await.ok().ok_or(Status::InternalServerError)
+    UploadResponder::Success
 }
 
 #[rocket::launch]
